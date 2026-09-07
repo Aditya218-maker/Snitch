4890d9dea01256b821f171151caab1b25e70a4f2
Aditya218-maker <aditya777anand@gmail.com>
2026-09-07 11:48:58 +0530

    Add user authentication with validation and token handling
    - Implement user registration and login functionality in auth.controller.js
    - Add validation for registration and login requests using express-validator
    - Update auth.routes.js to include validation middleware
    - Modify package.json and package-lock.json to include necessary dependencies

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
+import { validateLoginUser, validateRegisterUser } from "../validator/auth.validator";
 
 const router = Router();
 
-router.post('/register', register)
+router.post('/register', validateRegisterUser, register)
+
+router.post('/login', validateLoginUser, login)
+
+ 
 
 export default router;
