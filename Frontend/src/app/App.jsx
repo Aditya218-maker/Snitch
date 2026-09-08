258ce161828db5eba7d234a9af403bbeb6481953
Aditya218-maker <aditya777anand@gmail.com>
2026-09-08 11:07:46 +0530

    feat: add react-router for routing and create Register page

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import { RouterProvider } from "react-router"
+import { routes } from "./app.routes"
 
 const App = () => {
   return (
     <>
+    <RouterProvider router = {routes} />
     </>
   )
 }
