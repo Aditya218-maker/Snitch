b3099f2d2f648e234869c21d465c8ab937400826
Aditya218-maker <aditya777anand@gmail.com>
2026-09-08 18:22:47 +0530

    feat: add CORS middleware for handling cross-origin requests and implement getMe function in auth controller

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -73,3 +73,19 @@ export const login = async (req, res) => {
 
   await sendTokenResponse(user, res, 'User logged in successfully')
 }
+
+export const getMe = async (req, res) => {
+    const user = req.user;
+
+    res.status(200).json({
+        message: "User fetched successfully",
+        success: true,
+        user: {
+            id: user._id,
+            email: user.email,
+            contact: user.contact,
+            fullname: user.fullname,
+            role: user.role
+        }
+    })
+}
