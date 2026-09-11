353677d10e22024ea427a9ee2a4067eae163a808
Aditya218-maker <aditya777anand@gmail.com>
2026-09-11 09:57:36 +0530

    fix: add missing checks for GOOGLE_CLIENT_ID and correct typo in GOOGLE_CLIENT_SECRET error message

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -5,6 +5,14 @@ if(!process.env.MONGO_URI) {
     throw new Error("MONGO_URI is not defined in environment variables")
 }
 
+if(!process.env.GOOGLE_CLIENT_ID) {
+    throw new Error("GOOGLE_CLIENT_ID is not defined in environment variables")
+}
+
+if(!process.env.GOOGLE_CLIENT_SECRET) {
+    throw new Error("GOOGLE_CLIENT_SECRET is not dfeined in environment variables")
+}
+
 export const config = {
     MONGO_URI : process.env.MONGO_URI,
     JWT_SECRET: process.env.JWT_SECRET,
