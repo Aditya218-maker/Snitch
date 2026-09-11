3b4197eb974deeab6d083a5b5c4b50dff95431c1
Aditya218-maker <aditya777anand@gmail.com>
2026-09-11 09:35:01 +0530

    feat: add .gitignore for environment variables and update config.js to include JWT and Google credentials

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -6,6 +6,9 @@ if(!process.env.MONGO_URI) {
 }
 
 export const config = {
-    MONGO_URI : process.env.MONGO_URI
+    MONGO_URI : process.env.MONGO_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SEC
 }
 
