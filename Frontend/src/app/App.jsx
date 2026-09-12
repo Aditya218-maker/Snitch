77dc0c7f2a2bba86cb1f62cd31813a95a3251c3f
Aditya218-maker <aditya777anand@gmail.com>
2026-09-12 10:15:57 +0530

    fix: add missing semicolon in App.css and clean up App.jsx formatting

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,10 +1,27 @@
-import { RouterProvider } from "react-router"
-import { routes } from "./app.routes"
+import './App.css'
+import { RouterProvider } from 'react-router'
+import { routes } from './app.routes'
+import { useSelector } from 'react-redux'
+import { useAuth } from '../features/auth/hook/useAuth'
+import { useEffect } from 'react'
 
-const App = () => {
+
+function App() {
+
+
+  const { handleGetMe } = useAuth()
+
+  const user = useSelector(state => state.auth.user)
+
+  console.log(user)
+
+  useEffect(() => {
+    handleGetMe()
+  }, [])
+
   return (
     <>
-    <RouterProvider router = {routes} />
+      <RouterProvider router={routes} />
     </>
   )
 }
