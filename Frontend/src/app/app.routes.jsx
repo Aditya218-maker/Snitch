8cb406b8c9a25eee41d6fd13a65ea75335e46bb7
Aditya218-maker <aditya777anand@gmail.com>
2026-09-18 10:39:31 +0530

    feat: update root route to render Home component instead of placeholder

--- Frontend/src/app/app.routes.jsx
+++ Frontend/src/app/app.routes.jsx
@@ -4,11 +4,12 @@ import Login from "../features/auth/pages/Login.jsx";
 import CreateProduct from "../features/products/pages/CreateProduct.jsx";
 import Dashboard from "../features/products/pages/Dashboard.jsx";
 import Protected from "../features/auth/components/Protected.jsx";
+import Home from "../features/products/pages/Home.jsx";
 
 export const routes = createBrowserRouter([
     {
         path: "/",
-        element: <h1>Amen</h1>
+        element: <Home />
     }, 
     {
         path: "/register",
