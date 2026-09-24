30a879cd3fbd3c1ff8268ade0bb80048ad454507
Aditya218-maker <aditya777anand@gmail.com>
2026-09-24 11:27:17 +0530

    feat: add cart routes and import authentication middleware

---
+++ Backend/src/controllers/cart.contoller.js
@@ -0,0 +1,1 @@
+import cartmodel from "../models/cart.model.js"
--- Backend/src/routes/cart.routes.js
+++ Backend/src/routes/cart.routes.js
@@ -1,5 +1,16 @@
 import express from "express"
+import { authenticateUser } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
+
+/**
+ * @route POST /api/cart/add/:productId/:variantId
+ * @desc Add item to cart
+ * @access Private
+ * @argument productId - ID of the product to add
+ * @argument variantId - ID of the variant to add
+ * @argument quantity - Quantity of the item to add (optional, default: 1)
+ */
+router.post("/add/:productId/:variantId", authenticateUser, validateAddToCart, addToCart)
 
 export default router;
