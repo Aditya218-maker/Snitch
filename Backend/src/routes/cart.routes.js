29bf238cd3b9d85acd2c40d9443e9fc547a8473a
Aditya218-maker <aditya777anand@gmail.com>
2026-09-25 09:13:35 +0530

    feat: enhance addToCart function to handle stock validation and update cart items

--- Backend/src/routes/cart.routes.js
+++ Backend/src/routes/cart.routes.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { authenticateUser } from "../middlewares/auth.middleware.js";
 import { validateAddToCart } from "../validator/cart.validator.js";
+import { addToCart } from "../controllers/cart.contoller.js";
 
 const router = express.Router();
 
