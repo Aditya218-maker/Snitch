cf935b1e8f404c33975e40419dcf04976a5da421
Aditya218-maker <aditya777anand@gmail.com>
2026-09-25 10:14:11 +0530

    fix: update import path for useCart hook in Cart component

--- Frontend/src/app/app.routes.jsx
+++ Frontend/src/app/app.routes.jsx
@@ -1,48 +1,68 @@
-import { createBrowserRouter } from "react-router";
-import Register from "../features/auth/pages/Register.jsx";
-import Login from "../features/auth/pages/Login.jsx";
-import CreateProduct from "../features/products/pages/CreateProduct.jsx";
-import Dashboard from "../features/products/pages/Dashboard.jsx";
-import Protected from "../features/auth/components/Protected.jsx";
-import Home from "../features/products/pages/Home.jsx";
-import ProductDetail from "../features/products/pages/ProductDetail.jsx";
-import SellerProductdetails from "../features/products/pages/SellerProductdetails.jsx";
-
+import { createBrowserRouter } from 'react-router'
+import Register from '../features/auth/pages/Register.jsx'
+import Login from '../features/auth/pages/Login.jsx'
+import CreateProduct from '../features/products/pages/CreateProduct.jsx'
+import Dashboard from '../features/products/pages/Dashboard.jsx'
+import Protected from '../features/auth/components/Protected.jsx'
+import Home from '../features/products/pages/Home.jsx'
+import ProductDetail from '../features/products/pages/ProductDetail.jsx'
+import SellerProductdetails from '../features/products/pages/SellerProductdetails.jsx'
+import Cart from '../features/cart/pages/Cart.jsx'
 
 export const routes = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />
-    }, 
-    {
-        path: "/register",
-        element: <Register />
-    },
-    {
-        path: "/login",
-        element: <Login/>
-    },
-    {
-        path: "/product/:productId",
-        element: <ProductDetail />
-    },
-    {
-        path: "/seller",
-        //perform nesting as we will have multiple APIs starting with /seller
-        children : [
-            {
-                path: "/seller/create-product",
-                element: <Protected role="seller" ><CreateProduct /></Protected>
-            },
-            {
-                path: "/seller/dashboard",
-                element: <Protected role="seller"><Dashboard /></Protected>
-            },
-            {
-                path: "/seller/product/:productId",
-                element: <Protected role="seller"> <SellerProductdetails/></Protected>
-            }
-        ]
-    }
-])
+  {
+    path: '/',
+    element: <Home />
+  },
 
+  {
+    path: '/register',
+    element: <Register />
+  },
+
+  {
+    path: '/login',
+    element: <Login />
+  },
+
+  {
+    path: '/product/:productId',
+    element: <ProductDetail />
+  },
+
+  {
+    path: '/cart',
+    element: <Protected><Cart /></Protected>
+  },
+  {
+    path: '/seller',
+    //perform nesting as we will have multiple APIs starting with /seller
+    children: [
+      {
+        path: '/seller/create-product',
+        element: 
+          <Protected role='seller'>
+            <CreateProduct />
+          </Protected>
+    
+      },
+      {
+        path: '/seller/dashboard',
+        element: 
+          <Protected role='seller'>
+            <Dashboard />
+          </Protected>
+        
+      },
+      {
+        path: '/seller/product/:productId',
+        element: 
+          <Protected role='seller'>
+            {' '}
+            <SellerProductdetails />
+          </Protected>
+    
+      }
+    ]
+  }
+])
